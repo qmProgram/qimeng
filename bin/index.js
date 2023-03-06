ec30f0c7693ed224e5746aed9175dccc6b8f3140
VitoWxy <71478598+wang-xin-yuan-001@users.noreply.github.com>
2023-03-06 12:44:45 +0800

    init

---
+++ bin/index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const program = require("commander"); //用于捕获命令
+const chalk = require("chalk"); //用于字体加色
+const download = require("download-git-repo"); //用于下载git的包
+const inquirer = require("inquirer"); //用于与用户输入做交互
+const symbols = require("log-symbols"); //信息前面加✔或✖
+
+program
+  .version(require("../package").version, "-v,--version")
+  .command("init <name>")
+  .action((name) => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "Dear",
+          message: "请输入你的名字",
+        },
+      ])
+      .then((answers) => {
+        console.log(`Dear ${answers.Dear} 项目将继续完善~你愿意加入其中嘛`);
+        download(
+          "direct:https://github.com:wang-xin-yuan-001/NodeTemplate",
+          name,
+          // { clone: true },
+          function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(symbols.success, chalk.green("创建项目成功"));
+            }
+          }
+        );
+      });
+  });
+
+program.parse(process.argv);
